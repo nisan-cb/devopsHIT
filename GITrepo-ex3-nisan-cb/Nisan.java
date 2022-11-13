246beca886e312423a1fe170728113526cbfe158
Nisan <nnisan858@gmail.com>
2022-11-13 19:43:57 +0200

    change file

--- GITrepo-ex3-nisan-cb/Nisan.java
+++ GITrepo-ex3-nisan-cb/Nisan.java
@@ -1,7 +1,7 @@
 public class Nisan {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Nisan Cohen Burayev");
+            System.out.println("Nisan Cohen Burayev !!!!");
         }
     }
 }
